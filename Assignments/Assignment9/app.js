96e0cdbecf0905db391a907c7a4e65d0b206aa2d
khuzaimashoaib <khuzaima.shoaib@gmail.com>
2025-07-11 04:13:24 +0500

    Assignment 9 Completed

--- Assignments/Assignment9/app.js
+++ Assignments/Assignment9/app.js
@@ -4,6 +4,8 @@ const lowercaseCheckbox = document.getElementById("lowercase");
 const numbersCheckbox = document.getElementById("numbers");
 const specialCheckbox = document.getElementById("special");
 const passwordOutput = document.getElementById("passwordOutput");
+const passStrength = document.getElementById("pass_strength");
+var errorMessage = "Please select at least one character type.";
 
 const upperChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const lowerChars = "abcdefghijklmnopqrstuvwxyz";
@@ -22,9 +24,37 @@ function generatePassword() {
   if (includeLowercase) charSet += lowerChars;
   if (includeNumbers) charSet += numberChars;
   if (includeSpecial) charSet += specialChars;
+  if (length <= 3) {
+    passStrength.innerHTML = "Very Weak";
+    passStrength.style.backgroundColor = "#d93511";
+    passStrength.style.color = "#fff";
+  } else if (length <= 6) {
+    passStrength.innerHTML = "Weak";
+    passStrength.style.backgroundColor = "#ffb370";
+    passStrength.style.color = "#000";
+  } else if (length <= 8) {
+    passStrength.innerHTML = "Good";
+    passStrength.style.backgroundColor = "#ffddbf";
+    passStrength.style.color = "#000";
+  } else if (length <= 10) {
+    passStrength.innerHTML = "Strong";
+    passStrength.style.backgroundColor = "#d5f2a5";
+    passStrength.style.color = "#000";
+  } else {
+    passStrength.innerHTML = "Very Strong";
+    passStrength.style.backgroundColor = "#9ae437";
+    passStrength.style.color = "#000";
+  }
 
   if (charSet === "") {
-    alert("Please select at least one character type.");
+    passwordOutput.style.color = "red";
+    passwordOutput.style.fontSize = "12px";
+    passwordOutput.value = errorMessage;
+    setTimeout(() => {
+      passwordOutput.value = "";
+      passwordOutput.style.color = "";
+      passwordOutput.style.fontSize = "";
+    }, 1500);
     return;
   }
 
@@ -40,7 +70,14 @@ function generatePassword() {
 
 // copy button event
 function copyPassword() {
+  var originalPassword = passwordOutput.value;
+
   navigator.clipboard.writeText(passwordOutput.value);
-  alert("Password copied!");
+  passwordOutput.style.color = "green";
+  passwordOutput.value = "Password copied!";
+  setTimeout(() => {
+    passwordOutput.style.color = "";
+    passwordOutput.value = originalPassword;
+  }, 1200);
 }
 
